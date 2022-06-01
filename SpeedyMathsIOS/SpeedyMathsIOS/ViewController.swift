4deb0bcf270a45c17415820593ab68e0c86ca3ac
AryaaSk <aryaa_saravana@outlook.com>
2022-06-01 21:38:21 +0100

    Implemented basic webview to display website

--- SpeedyMathsIOS/SpeedyMathsIOS/ViewController.swift
+++ SpeedyMathsIOS/SpeedyMathsIOS/ViewController.swift
@@ -6,12 +6,78 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
 
+    //Still need to create App Logos and then hopefully it gets approved by the App Store
+    
+    @IBOutlet var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        webView.configuration.preferences.javaScriptEnabled = true
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        let url = URL(string: "https://speedymaths-b7f6e.web.app")!
+        let urlRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+    }
+    
+    
+    //MARK: Code used for the alerts and prompts inside of the website (Javascript)
+    //https://stackoverflow.com/questions/34185339/wkwebview-javascript-confirm-and-alert-not-working
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            completionHandler()
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            completionHandler(true)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
+            completionHandler(false)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        
+        let alertController = UIAlertController(title: nil, message: prompt, preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.text = defaultText
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            if let text = alertController.textFields?.first?.text {
+                completionHandler(text)
+            } else {
+                completionHandler(defaultText)
+            }
+            
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in
+            
+            completionHandler(nil)
+            
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 
 
